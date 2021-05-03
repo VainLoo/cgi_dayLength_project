08db5cf4715b1aba56f4ecf356b843232a840483
Allan Loo <loo_allan@yahoo.com>
2021-05-03 09:19:13 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,49 @@
 <template>
-  <div class="wrapper">
-    <div class="box a">
-      <h1>Day Lenght Calculator</h1>
-    </div>
+  <div>
+    <div class="wrapper">
+      <div class="box a">
+        <h1>Day Lenght Calculator</h1>
+      </div>
       <DataBox @updates="update" ref="dataBox" :latLong="latLng" />
-      <ChartBox ref="chartBox" :latLng="latLng"  />
-      <MapBox @newCords="update" ref="mapBox" :latLong="latLng"/>
+      <ChartBox ref="chartBox" :latLng="latLng" />
+      <MapBox @newCords="update" ref="mapBox" :latLong="latLng" />
+    </div>
+    <footer>
+      <p>Author: Allan Loo</p>
+      <p>loo_allan@yahoo.com</p>
+    </footer>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-      latLng: { //For storing latitude/longitude values
+      latLng: {
+        //For storing latitude/longitude values
         lat: 58.365,
         lng: 26.743,
-      }
+      },
     };
   },
   methods: {
     //Causes updates to be made.
-    update() { 
+    update() {
       this.$refs.dataBox.calcDayLenght();
       this.$refs.chartBox.redoChart();
-    }
-
-  }
+    },
+  },
 };
 </script>
 
 <style>
 .a {
   grid-column: 1 / span 2;
+}
+
+footer {
+  text-align: center;
+  padding: 3px;
 }
 
 @media only screen and (max-width: 768px) {
@@ -49,7 +59,5 @@ export default {
     grid-column: 1;
     grid-row: 1;
   }
- 
 }
-
 </style>
